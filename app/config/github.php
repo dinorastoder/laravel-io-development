4797e6660e7f455d2891f6caec516d42164b3d60
Dino <d.rastoder@sportradar.com>
2014-08-01 19:12:53 +0200

    Working app on local

---
+++ app/config/github.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'client_id'     => '4a93193d11469fe234b2',
+    'client_secret' => '048a8625ede278ddfae5da3fd46b28779be7bef3',
+    'scope'         => ['user:email'],
+    'redirect_url'  => 'http://io.local:8000/login',
+];
